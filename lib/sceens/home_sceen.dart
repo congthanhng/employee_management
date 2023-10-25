0347f240524c975d75f65030ff322f56542ff383
NguyenNhatHau <110582814+NguyenNhatHau14@users.noreply.github.com>
2023-10-25 18:35:49 +0700

    fix: CKTextStyle

--- lib/sceens/home_sceen.dart
+++ lib/sceens/home_sceen.dart
@@ -1,4 +1,5 @@
 
+import 'package:employee_management/themes/extension/fonts/employee_management_font_extension.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
--- lib/themes/extension/fonts/employee_management_font_extension.dart
+++ lib/themes/extension/fonts/employee_management_font_extension.dart
@@ -1,26 +1,24 @@
-import 'dart:ui';
 import 'package:employee_management/themes/extension/fonts/employee_management_font.dart';
 import 'package:flutter/material.dart';
 
 extension EmployeeManagementExtension on BuildContext {
-  TextStyle get subText => EmployeeManagementFont.subText as TextStyle;
+  TextStyle get subText => EmployeeManagementFont.subText;
 
-  TextStyle get textWarningFont => EmployeeManagementFont.textWarning as TextStyle;
+  TextStyle get textWarning => EmployeeManagementFont.textWarning;
 
-  TextStyle get textField => EmployeeManagementFont.textField as TextStyle;
+  TextStyle get textField => EmployeeManagementFont.textField;
 
-  TextStyle get textStatus => EmployeeManagementFont.textStatus as TextStyle;
+  TextStyle get textStatus => EmployeeManagementFont.textStatus;
 
-  TextStyle get textDrawer => EmployeeManagementFont.textDrawer as TextStyle;
+  TextStyle get textDrawer => EmployeeManagementFont.textDrawer;
 
-  TextStyle get textLocation =>
-      EmployeeManagementFont.textLocation as TextStyle;
+  TextStyle get textLocation => EmployeeManagementFont.textLocation;
 
-  TextStyle get textTitle => EmployeeManagementFont.textTitle as TextStyle;
+  TextStyle get textTitle => EmployeeManagementFont.textTitle;
 
-  TextStyle get textDate => EmployeeManagementFont.textDate as TextStyle;
+  TextStyle get textDate => EmployeeManagementFont.textDate;
 
-  TextStyle get textName => EmployeeManagementFont.textName as TextStyle;
+  TextStyle get textName => EmployeeManagementFont.textName;
 
-  TextStyle get textTime => EmployeeManagementFont.textTime as TextStyle;
+  TextStyle get textTime => EmployeeManagementFont.textTime;
 }
